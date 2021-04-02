9d343e801b45d9fb5374fd6585ecb44fb571274d
kivyfreakt <yaloveganikita@outlook.com>
2021-04-03 00:11:26 +0300

    Add some tree methods

--- algos/semester4/task3/avl_tree.h
+++ algos/semester4/task3/avl_tree.h
@@ -10,35 +10,35 @@ using namespace std;
 // УЗЕЛ ДЕРЕВА
 struct node
 {
-    char delta; // баланс (разность поддеревьев узла)
     int key; // ключ узла
-    node *nodes[2]; // левое и правое поддерево
+    signed char balance_factor; // баланс (разность поддеревьев узла)
+    node* nodes[2]; // левое и правое поддерево
 
     node(int k): key(k), delta(0){nodes[0] = nodes[1] = nullptr;}
-    node(const node &) = delete;
-    node(){delete nodes[1]; delete nodes[0];}
+    node(const node& ) = delete;
+    ~node(){delete nodes[1]; delete nodes[0];}
 };
 
 using Stack = stack<pair<Node*, int>>;
 
-//ИТЕРАТОР ЧТЕНИЯ — нужны сравнения, разыменования, инкремент
-struct myiter: public std::iterator<std::forward_iterator_tag, int>
+//ИТЕРАТОР ЧТЕНИЯ (нужны сравнения, разыменования, инкремент)
+struct tree_iterator: public std::iterator<std::forward_iterator_tag, int>
 {
-    node * ptr; //Реальный указатель на элемент контейнера
+    node* ptr; //Реальный указатель на элемент контейнера
     Stack st; // стек с путем от корня дерева
 
-    myiter(node *p = nullptr) : ptr(p) {}
-    myiter(node *p = nullptr, const Stack &&St) : ptr(p), st(move(St)) {}
-    bool operator== (const myiter & other) const { return ptr == other.ptr; }
-    bool operator!= (const myiter & other) const { return !(*this == other); }
-    // bool operator != (const myiter & other) const { return ptr != other.ptr;}
-    myiter operator++(); //Ключевая операция — инкремент по контейнеру
-    myiter operator++(int) { myiter temp(*this); ++*this; return temp;}
+    tree_iterator(node *p = nullptr) : ptr(p) {}
+    tree_iterator(node *p = nullptr, const Stack &&St) : ptr(p), st(move(St)) {}
+
+    bool operator== (const tree_iterator & other) const { return ptr == other.ptr; }
+    bool operator!= (const tree_iterator & other) const { return !(*this == other); }
+    tree_iterator operator++(); // Инкремент по контейнеру
+    tree_iterator operator++(int) { tree_iterator temp(*this); ++*this; return temp; }
     pointer operator->() { return &ptr->key; } //Разыменование косвенное
     reference operator*(){ return ptr->key; } //Разыменование прямое
 };
 
-myiter& myiter::operator++()
+tree_iterator& tree_iterator::operator++()
 {
     if (!ptr)
         return *this;
@@ -67,35 +67,33 @@ myiter& myiter::operator++()
     return *this;
 }
 
-//ИТЕРАТОР ВСТАВКИ
-template<typename Container, typename Iter = myiter>
-struct outiter: public std::iterator<std::output_iterator_tag, typename Container::value_type>
+// ИТЕРАТОР ВСТАВКИ (нужны присваивание, разыменование , инкремент)
+template<typename Container, typename Iter = tree_iterator>
+class outiterator: public std::iterator<std::output_iterator_tag, typename Container::value_type>
 {
     protected:
         Container& container; // Контейнер для вставки элементов
         Iter iter; // текущее значение итератора чтения
     public:
-        outiter(Container &c, Iter it) : container(c), iter(it) {}
-        const outiter<Contaiter>& operator= (const typename Contaiter::value_type& value)
+        outiterator(Container &c, Iter it) : container(c), iter(it) {}
+        const outiterator<Contaiter>& operator= (const typename Contaiter::value_type& value)
         {
             iter = container.insert(iter, value).first;
             return *this;
         }
 
-        const outiter<Contaiter>& operator= (const outiter<Contaiter> &) // Присваивание копии фиктивное
-        {
-            return *this
-        }
-        outiter<Contaiter>& operator* () {return *this} // Разыменование - пустая операция
-        outiter<Contaiter>& operator++ () {return *this} // Инкремент - пустая операция
-        outiter<Contaiter>& operator++ (int) {return *this} // Инкремент - пустая операция
+        // Присваивание копии фиктивное
+        const outiterator<Contaiter>& operator= (const outiterator<Contaiter> &) { return *this }
+        outiterator<Contaiter>& operator* () {return *this} // Разыменование - пустая операция
+        outiterator<Contaiter>& operator++ () {return *this} // Инкремент - пустая операция
+        outiterator<Contaiter>& operator++ (int) {return *this} // Инкремент - пустая операция
 };
 
 // функция для создания итератора вставки
 template<typename Container, typename Iter>
-inline outiter<Container, Iter> outinserter(Container& c, Iter It)
+inline outiterator<Container, Iter> out_inserter(Container& c, Iter It)
 {
-    return outiter<Container, Iter>(c, it);
+    return outiterator<Container, Iter>(c, it);
 }
 
 // АВЛ ДЕРЕВО
@@ -106,39 +104,39 @@ class tree
     node *root;
     size_t h, count; // высота и мощность дерева
 
+    node* find_element(node*, int) const;
     public:
         using key_type = int;
         using value_type = int;
         using key_compare = less<int>;
 
-        void swap(tree & other)
-        {
-            std::swap(tag, other.tag);
-            std::swap(root, other.root);
-            std::swap(h, other.h);
-            std::swap(count, other.count);
-        }
+        // allocation/deallocation:
+        tree(): tag('A' + tags++), root(nullptr), h(0), n(0) {}
+        template<typename it>
+        tree(it ibegin, it iend): tree() {for(auto x = ibegin; x != iend; ++x) insert(*x);}
+        tree(const tree &other): tree() {for(auto x = other.begin(); x != other.end(); ++x) insert(*x);}
+        tree(tree && other): tree() { swap(other); }
+        ~tree(){ delete root; }
+        void swap(tree &);
 
-        myiter insert(const myiter& where, const int& k);
-        myiter insert(const int& k, const myiter& where = myiter(nullptr))
-        {return insert(where, k).first;}
+        // accessors:
+        tree_iterator begin() const;
+        tree_iterator end() const { return tree_iterator(nullptr); }
+        bool empty() const { return (root == nullptr); }
+        // bool empty() const { return (count == 0); }
+        int size() const { return count; }
+
+        // insert/erase
+        pair<tree_iterator, bool> insert(int, tree_iterator = tree_iterator(nullptr))
+        tree_iterator insert(const tree_iterator& where, const int& k);
+        tree_iterator insert(const int& k, const tree_iterator& where = tree_iterator(nullptr))
+        { return insert(where, k).first; }
+        pair<tree_iterator, bool> erase(int);
+
+        // set operations:
         void display(int = 1);
-        myiter begin() const;
-        myiter end() const {return myiter(nullptr)}
-        pair<myiter, bool> insert(int, myiter = myiter(nullptr))
-        pair<myiter, bool> remove(int);
-        int size(){return count;}
-        myiter find(int) const;
-        bool empty() { return(root == nullptr); }
-
-        tree & operator= (const tree & other)
-        {
-            tree temp;
-            for (auto x: other)
-                temp.insert(x);
-            swap(temp);
-            return *this;
-        }
+        tree_iterator find(int);
+        tree & operator= (const tree &);
         tree & operator= (tree && other) {swap(other); return *this;}
         tree & operator|= (const tree &);
         tree operator| (const tree & other) const {tree res(*this); return (res |= other);}
@@ -148,16 +146,17 @@ class tree
         tree operator- (const tree & other) const {tree res(*this); return (res -= other);}
         tree & operator^= (const tree &);
         tree operator^ (const tree & other) const {tree res(*this); return (res ^= other);}
-
-        tree(): tag('A' + tags++), root(nullptr), h(0), n(0) {}
-        template<typename it>
-        tree(it, it); // формирование ддп из отрезка
-        tree(const tree &other): tree() {for(auto x = other.begin(); x != other.end(); ++x) insert(*x);}
-        tree(tree && other): tree() {swap(other);}
-        ~tree(){delete root;}
 }
 
-myiter tree::begin() const
+void swap(tree & other)
+{
+    std::swap(tag, other.tag);
+    std::swap(root, other.root);
+    std::swap(h, other.h);
+    std::swap(count, other.count);
+}
+
+tree_iterator tree::begin() const
 {
     Stack st;
     Node *p(root);
@@ -171,7 +170,37 @@ myiter tree::begin() const
         // for(;p->nodes[0];p = p->nodes[0]) st.push(make_pair(p, 0));
     }
 
-    return myiter(p, move(st));
+    return tree_iterator(p, move(st));
+}
+
+node* find_element(node* _node, int value) const
+{
+    if (_node == nullptr ||
+        (!(value < _node->value) && !(_node->value < value)))
+        return _node;
+    else if (value < _node->value)
+        return find_element(_node->left, value);
+    else
+        return find_element(_node->right, value);
+}
+
+tree_iterator tree::find(int value)
+{
+    node* element = find_element(root, value);
+    if (element == nullptr)
+        return end();
+    else
+        return tree_iterator(element);
+}
+
+
+tree & operator= (const tree & other)
+{
+    tree temp;
+    for (auto x: other)
+        temp.insert(x);
+    swap(temp);
+    return *this;
 }
 
 #endif
