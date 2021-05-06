99e647715290e140e6ac5c373c192fbfa3530a5a
kivyfreakt <yaloveganikita@outlook.com>
2021-05-07 00:26:10 +0300

    Start task4 algos

---
+++ algos/semester4/task4/avl_tree.h
@@ -0,0 +1,407 @@
+#ifndef AVL_TREE_H
+#define AVL_TREE_H
+
+using namespace std;
+
+const int B[] = {-1,1};
+
+// УЗЕЛ ДЕРЕВА
+struct node
+{
+    int key; // ключ узла
+    signed char balance_factor; // баланс (разность поддеревьев узла)
+    int duplicates; // количество дубликатов ключа
+    node* nodes[2]; // левое и правое поддерево
+
+    node(int k): key(k), balance_factor(0), duplicates(0) {nodes[0] = nodes[1] = nullptr;}
+    node(const node&) = delete;
+    ~node(){delete nodes[1]; delete nodes[0];}
+};
+
+using Stack = stack<pair<node*, int>>;
+
+//ИТЕРАТОР ЧТЕНИЯ (нужны сравнения, разыменования, инкремент)
+struct tree_iterator: public std::iterator<std::forward_iterator_tag, int>
+{
+    node* ptr; //Реальный указатель на элемент контейнера
+    Stack st; // стек с путем от корня дерева
+
+    tree_iterator(node *p = nullptr) : ptr(p) {}
+    tree_iterator(node *p, Stack s) : ptr(p), st(s) {}
+    bool operator== (const tree_iterator & other) const { return ptr == other.ptr; }
+    bool operator!= (const tree_iterator & other) const { return !(*this == other); }
+    tree_iterator& operator++(); // Инкремент по контейнеру
+    tree_iterator operator++(int) { tree_iterator temp(*this); ++*this; return temp; }
+    pointer operator->() { return &ptr->key; } //Разыменование косвенное
+    reference operator*(){ return ptr->key; } //Разыменование прямое
+};
+
+tree_iterator& tree_iterator::operator++()
+{
+    if (!ptr)
+        return *this;
+
+    if (ptr->nodes[1])
+    {
+        st.push(make_pair(ptr, 1));
+        ptr = ptr->nodes[1];
+        for(;ptr->nodes[0];ptr = ptr->nodes[0]) st.push(make_pair(ptr, 0));
+    }
+    else
+    {
+        pair <node*, int> pp(ptr, 1);
+        while (!st.empty() && pp.second) {pp = st.top(); st.pop();}
+        if (pp.second)
+            ptr = nullptr;
+        else
+            ptr = pp.first;
+    }
+
+    return *this;
+}
+
+// АВЛ ДЕРЕВО
+class tree
+{
+    node *root; // корень дерева
+    int h, count; // высота и мощность дерева
+
+    node* find_element(node*, int) const;
+    public:
+        // конструкторы/деструкторы:
+        tree(): root(nullptr), h(0), count(0) {}
+        tree(const tree &other): tree() {for(auto x = other.begin(); x != other.end(); ++x) insert(*x);}
+        tree(tree && other): tree() {swap(other);}
+        ~tree(){ delete root; }
+        void swap(tree &);
+
+        // доступ:
+        tree_iterator begin() const;
+        tree_iterator end() const { return tree_iterator(nullptr); }
+        bool empty() const { return (root == nullptr); }
+        int size() const { return count; }
+        int height() const {return h;}
+
+        // вставка/удаление
+        pair<tree_iterator, bool> insert(int, tree_iterator = tree_iterator(nullptr));
+        tree_iterator insert(const tree_iterator& where, const int& k) { return insert(k, where).first; }
+        bool erase(int);
+
+        // другие методы
+        tree_iterator find(int) const;
+        tree & operator= (const tree &);
+        tree & operator= (tree && other) {swap(other); return *this;}
+};
+
+void tree::swap(tree & other)
+{
+    std::swap(root, other.root);
+    std::swap(h, other.h);
+    std::swap(count, other.count);
+}
+
+tree_iterator tree::begin() const
+{
+    Stack st;
+    node *p(root);
+    if (p)
+        for(;p->nodes[0];p = p->nodes[0]) st.push(make_pair(p, 0));
+
+    return tree_iterator(p, move(st));
+}
+
+node* tree::find_element(node* _node, int key) const
+{
+    if (_node == nullptr ||
+        (!(key < _node->key) && !(_node->key < key)))
+        return _node;
+    else
+        return find_element(_node->nodes[!(key < _node->key)], key);
+}
+
+tree_iterator tree::find(int value) const
+{
+    node* element = find_element(root, value);
+    if (element == nullptr)
+        return end();
+    else
+        return tree_iterator(element);
+}
+
+pair<tree_iterator, bool> tree::insert(int k, tree_iterator where)
+{
+    node *q{root}, *p;
+    Stack St;
+
+    if (!where.ptr) // свободная вставка
+    {
+        if (empty())
+        {
+            root = new node(k);
+            count = h = 1;
+            St.push(make_pair(root, 1));
+            return make_pair(tree_iterator(root, move(St)), true);
+        }
+    }
+    else // с места предыдущей вствки
+    {
+        q = where.ptr;
+        St = move(where.st);
+    }
+
+    while (q) // поиск места вставки...
+    {
+        if (k == q->key)
+        {
+            ++(q->duplicates);
+            return (make_pair(tree_iterator(q, move(St)), false));
+        }
+        p = q;
+        int a{k < q->key}; //Шаг поиска вниз по дереву
+        if (a)
+            q = q->nodes[0];
+        else
+            q = q->nodes[1];
+        St.push(make_pair(p, 1 - a));
+    }
+
+    q = new node(k); //Вставлен новый узел в точке q
+    if (p)
+    {
+        if (k < p->key)
+            p->nodes[0] = q;
+        else
+            p->nodes[1] = q;
+    }
+    else
+        root = q;
+
+    node *new_node = q;
+
+    while (!St.empty())
+    //Движемся к корню, опрокидывая балансы, ищем не нуль
+    {
+        auto pa = St.top(); St.pop();
+        p = pa.first;
+        int a = pa.second;
+
+        if (!(p->balance_factor))
+        {
+            p->balance_factor = B[a]; //Замена 0 на +-1
+            if (p == root) //Дерево подросло
+            {
+                ++h;
+                break;
+            }
+            else
+                q = p;
+        }
+        else if (p->balance_factor == -B[a])
+        { // Сбалансировалось
+            p->balance_factor = 0; //Замена +-1 на 0
+            break;
+        }
+        // Перебалансировка
+        else if (p->balance_factor == q->balance_factor)
+        {	//Случай 1: Однократный поворот
+            p->nodes[a] = q->nodes[1 - a];
+            q->nodes[1 - a] = p;
+            p->balance_factor = q->balance_factor = 0;
+            if(p == root)
+                p = root = q;
+            else
+                St.top().first->nodes[St.top().second] = p = q;
+            break;
+        }
+        else
+        { //Случай 2: Двукратный поворот
+            node *r(q->nodes[1 - a]);
+            p->nodes[a] = r->nodes[1 - a];
+            q->nodes[1 - a] = r->nodes[a];
+            r->nodes[1 - a] = p;
+            r->nodes[a] = q;
+            if (r->balance_factor == B[a])
+            {
+                p->balance_factor = -B[a];
+                q->balance_factor = 0;
+            }
+            else if (r->balance_factor == -B[a])
+            {
+                 p->balance_factor = 0;
+                 q->balance_factor = B[a];
+            }
+            else
+            {
+                p->balance_factor = q->balance_factor = 0;
+            }
+            r->balance_factor = 0;
+            if (p == root)
+                p = root = r;
+            else
+                St.top().first->nodes[St.top().second] = p = r;
+            break;
+        }
+    }
+
+    ++count;
+    return make_pair(tree_iterator(new_node, move(St)), true);
+}
+
+
+bool tree::erase(int k)
+{
+    node *q{root}, *p = nullptr;
+    Stack St;
+    int a;
+
+    // найти узел
+    while (q != nullptr && q->key != k)
+    {
+        p = q;
+        a = (k < q->key);
+        q = q->nodes[1 - a];
+        St.push(make_pair(p, 1 - a));
+    }
+
+    if (q == nullptr)
+        return false;
+    else
+        St.push(make_pair(q, 1 - a));
+
+    // удалить узел
+    if (q->duplicates > 0)
+        --(q->duplicates);
+    else
+    {
+        if(q->nodes[0] == nullptr && q->nodes[1] == nullptr)
+        {
+            if (p == nullptr)
+                return false;
+            if(q == p->nodes[0])
+                p->nodes[0] = nullptr;
+            else
+                p->nodes[1] = nullptr;
+            St.pop();
+            q->nodes[0] = q->nodes[1] = nullptr;
+            delete q;
+        }
+        else if(q->nodes[1] == nullptr)
+        {
+            if (p == nullptr)
+                return false;
+            if (q == p->nodes[0])
+                p->nodes[0] = q->nodes[0];
+            else
+                p->nodes[1] = q->nodes[0];
+            St.pop();
+            q->nodes[0] = q->nodes[1] = nullptr;
+            delete q;
+        }
+        else if(q->nodes[0] == nullptr)
+        {
+            if (p == nullptr)
+                return false;
+            if (q == p->nodes[0])
+                p->nodes[0] = q->nodes[1];
+            else
+                p->nodes[1] = q->nodes[1];
+            St.pop();
+            q->nodes[0] = q->nodes[1] = nullptr;
+            delete q;
+        }
+        else
+        {
+            node *succ = q->nodes[1];
+
+            for(p = q; succ->nodes[0]; p = succ, succ = succ->nodes[0]) St.push(make_pair(succ, 0));
+            q->key = succ->key;
+            q->duplicates = succ->duplicates;
+
+            if (succ == p->nodes[0])
+                p->nodes[0] = succ->nodes[1];
+            else
+                p->nodes[1] = succ->nodes[1];
+
+            succ->nodes[0] = succ->nodes[1] = nullptr;
+            delete succ;
+        }
+
+        // восстановить баланс
+        do //Цикл, пока не дойдём до корня дерева
+        {
+            auto pp = St.top(); St.pop();	//Поднятие из стека указателя на отца и направления
+            p = pp.first; a = pp.second;
+
+            if (!p)
+                --h; // Дошли до корня. Уменьшаем высоту дерева
+            else if (p->balance_factor) //b != 0
+            {
+                if (p->balance_factor == B[a]) // Поддерево сбалансировалось. Идем к корню...
+                    p->balance_factor = 0;
+                else // p->b == -B[a]: Требуется перебалансировка
+                {
+                    node *r;
+                    r = q = p->nodes[1 - a];
+                    if (r->balance_factor == -p->balance_factor)  // Случай 2: двукратный поворот
+                    {
+                        r = r->nodes[a];
+                        p->nodes[1 - a] = r->nodes[a];
+                        q->nodes[a] = r->nodes[1 - a];
+                        r->nodes[a] = p;
+                        r->nodes[1 - a] = q;
+                        if (r->balance_factor)
+                        {
+                            if (r->balance_factor == B[a])
+                            {
+                                p->balance_factor = 0;
+                                q->balance_factor = -B[a];
+                            }
+                            else
+                            {
+                                p->balance_factor = B[a];
+                                q->balance_factor = 0;
+                            }
+                            r->balance_factor = 0;
+                        }
+                        else
+                            q->balance_factor = p->balance_factor = 0;
+                    }
+                    else // Случаи 1 и 3: однократный поворот
+                    {
+                        p->nodes[1 - a] = r->nodes[a];
+                        r->nodes[a] = p;
+                        if (r->balance_factor) // Случай 1: поддерево стало ниже...
+                        {
+                            r->balance_factor = p->balance_factor = 0;
+                        }
+                        else // Случай 3: высота поддерева не изменилась!
+                        {
+                            r->balance_factor = B[a];
+                            break;
+                        }
+                    }
+
+                    if (St.top().first)
+                        St.top().first->nodes[St.top().second] = r;
+                    else
+                        root = r;
+                }
+            }
+            else
+                p->balance_factor = -B[a];
+        } while (!St.empty());
+
+        --count;
+    }
+    return true;
+}
+
+tree & tree::operator= (const tree & other)
+{
+    tree temp;
+    for (auto x: other)
+        temp.insert(x);
+    swap(temp);
+    return *this;
+}
+#endif
---
+++ algos/semester4/task4/main.cpp
@@ -0,0 +1,384 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <iterator>
+#include <stack>
+#include <vector>
+#include <chrono>
+
+#include "avl_tree.h"
+
+const int lim = 1000;
+using Set = tree;
+using Fterator = tree_iterator;
+using Seq = std::vector<Fterator>;
+
+// ИТЕРАТОР ВСТАВКИ (нужны присваивание, разыменование , инкремент)
+template<typename Container, typename Fter = Fterator>
+class outiterator: public iterator<output_iterator_tag, typename Container::value_type>
+{
+    protected:
+        Container& container; // Контейнер для вставки элементов
+        Fter iter; // текущее значение итератора чтения
+    public:
+        outiterator(Container &c, Fter it) : container(c), iter(it) {}
+        const outiterator<Container>& operator= (const typename Container::value_type& value)
+        {
+            iter = container.insert(iter, value);
+            return *this;
+        }
+
+        // Присваивание копии фиктивное
+        const outiterator<Container>& operator= (const outiterator<Container> &) { return *this; }
+        outiterator<Container>& operator* () {return *this;} // Разыменование - пустая операция
+        outiterator<Container>& operator++ () {return *this;} // Инкремент - пустая операция
+        outiterator<Container>& operator++ (int) {return *this;} // Инкремент - пустая операция
+};
+
+// функция для создания итератора вставки
+template<typename Container, typename Fter>
+inline outiterator<Container, Fter> outinserter(Container& c, Fter Ft)
+{
+    return outiterator<Container, Fter>(c, Ft);
+}
+
+class set_seq
+/*
+    Комбинированная структура, хранящая множество с последовательностью.
+
+    Множество реализовано при помощи структуры АВЛ-Дерево.
+    Последовательность реализована при помощи вектора итераторов
+    на элементы множества.
+*/
+{
+    static size_t tags; // количетво тегов
+    char tag; // тег струтуры
+    Set set_; // множество
+    Seq seq_; // последовательность
+
+    public:
+        using key_type = int;
+        using value_type = int;
+        using key_compare = less<int>;
+
+        set_seq(): tag('A' + tags++) {}
+        set_seq(int);
+        set_seq(set_seq &&);
+        set_seq(const set_seq &);
+        template <typename iter>
+        set_seq(iter, iter);
+        set_seq& operator=(const set_seq &);
+        set_seq& operator=(set_seq &&);
+
+        pair<Fterator, bool> insert(int, Fterator);
+        Fterator insert(const Fterator& where, const int& k) { return insert(k, where).first; }
+
+        // операции над последовательностью
+        void erase(int, int);
+        void prepare_excl(const set_seq&);
+        void excl(const set_seq&);
+        void concat(const set_seq&);
+        void change(const set_seq&, int);
+
+        // операции над множеством
+        set_seq& operator &= (const set_seq &);
+        set_seq operator & (const set_seq & other) const
+        { set_seq result(*this); return (result &= other); }
+        set_seq& operator -= (const set_seq &);
+        set_seq operator - (const set_seq & other) const
+        {set_seq result(*this); return (result -= other);}
+        set_seq& operator ^= (const set_seq &);
+        set_seq operator ^ (const set_seq & other) const
+        {set_seq result(*this); return (result ^= other);}
+
+        void display();
+        int power() const { return seq_.size(); }
+
+        friend void prepare_and(set_seq&, set_seq&, const int);
+};
+
+set_seq::set_seq(int power) : set_seq()
+{
+    for(int i = 0; i < power; ++i)
+        seq_.push_back(set_.insert(std::rand()%(power*3)).first);
+}
+
+set_seq::set_seq (set_seq && source)
+    : tag(source.tag), set_(std::move(source.set_)), seq_(std::move(source.seq_)) {}
+
+set_seq::set_seq (const set_seq & source): tag(source.tag)
+{
+    for (auto x : source.seq_)
+        seq_.push_back(set_.insert(*x).first);
+}
+
+template <typename iter>
+set_seq::set_seq (iter b, iter e): set_seq()
+{
+    for (auto i = b; i != e; ++i)
+        seq_.push_back(set_.insert(*i).first);
+}
+
+
+pair<Fterator, bool> set_seq::insert(int key, Fterator it = nullptr)
+{
+    auto r = set_.insert(key, it);
+    seq_.push_back(r.first);
+    return r;
+}
+
+void set_seq::erase(int left, int right)
+/*
+    Из последовательности исключается часть,
+    ограниченная порядковыми номерами от left до right.
+*/
+{
+    int p = power();
+    left = std::min(left, p);
+    right = std::min(right+1, p);
+    if(left <= right)
+    {
+        Set temp;
+        Seq stemp;
+        for(int i = 0; i < left; ++i)
+            stemp.push_back(temp.insert(*seq_[i]).first);
+        for(int i = right; i < p; ++i)
+            stemp.push_back(temp.insert(*seq_[i]).first);
+        set_.swap(temp);
+        seq_.swap(stemp);
+    }
+}
+
+void set_seq::prepare_excl( const set_seq& other )
+{
+	int a = rand()%other.power(),
+    b = rand()%other.power();
+	if (b>a)
+    {
+		for (int x = a; x <= b; ++x)
+        {
+			auto y = other.seq_[x];
+            insert(*y);
+		}
+	}
+}
+
+
+void set_seq::excl(const set_seq & other)
+/*
+    Вторая последовательность исключается из
+    первой, если она является ее частью.
+*/
+{
+    int n = power(),
+        m = other.power();
+    if(m)
+        for (int p = 0; p < n; ++p)
+        {
+            bool f = true;
+            if(*seq_[p] == other.seq_[0].ptr->key)
+            {
+                int q = p, r = 0;
+                if (m > 1)
+                    do
+                    {
+                        ++q;
+                        ++r;
+                        int c(*seq_[q]), d(other.seq_[r].ptr->key);
+                        f &= c == d;
+                    }
+                    while ((r<m-1) && f);
+                if(f)
+                {
+                    Set temp;
+                    Seq stemp;
+                    for(int i = 0; i < p; ++i)
+                        stemp.push_back(temp.insert(*seq_[i]).first);
+                    for(int i = p+m; i < power(); ++i)
+                        stemp.push_back(temp.insert(*seq_[i]).first);
+                    set_.swap(temp);
+                    seq_.swap(stemp);
+                    break;
+                }
+            }
+        }
+}
+
+
+void set_seq::concat(const set_seq & other)
+/*
+    Вторая последовательность сцепляется с первой.
+*/
+{
+    for(auto x : other.seq_)
+        seq_.push_back(set_.insert(*x).first);
+}
+
+
+void set_seq::change(const set_seq & other, int pos = 0)
+/*
+    Вторая последовательность заменяет элементы
+    первой, начиная с заданной позиции pos.
+*/
+{
+    if(pos >= power())
+        concat(other);
+    else
+    {
+        Seq stemp(seq_.begin(), seq_.begin() + pos);
+        std::copy(other.seq_.begin(), other.seq_.end(), back_inserter(stemp));
+
+        int q = pos + other.power();
+        if (q < power())
+            std::copy(seq_.begin() + q, seq_.end(), back_inserter(stemp));
+
+        Set temp;
+        seq_.clear();
+        for (auto x : stemp)
+            seq_.push_back(temp.insert(*x).first);
+        set_.swap(temp);
+    }
+}
+
+set_seq& set_seq::operator= (const set_seq & other)
+{
+    set_seq temp;
+    for (auto x: other.seq_)
+        seq_.push_back(temp.set_.insert(*x).first);
+    seq_.swap(temp.seq_);
+    set_.swap(temp.set_);
+    return *this;
+}
+
+set_seq& set_seq::operator= (set_seq && other)
+{
+    seq_.swap(other.seq_);
+    set_.swap(other.set_);
+    return *this;
+}
+
+set_seq& set_seq::operator&= (const set_seq & other)
+{
+    set_seq temp;
+    set_intersection(set_.begin(), set_.end(),
+        other.set_.begin(), other.set_.end(),
+        outinserter(temp, Fterator(nullptr)));
+    set_.swap(temp.set_);
+    seq_.swap(temp.seq_);
+	return *this;
+}
+
+
+set_seq& set_seq::operator-= (const set_seq & other)
+{
+    set_seq temp;
+    set_difference(set_.begin(), set_.end(),
+        other.set_.begin(), other.set_.end(),
+        outinserter(temp, Fterator(nullptr)));
+    set_.swap(temp.set_);
+    seq_.swap(temp.seq_);
+	return *this;
+}
+
+set_seq& set_seq::operator^= (const set_seq & other)
+{
+    set_seq temp;
+    set_symmetric_difference(set_.begin(), set_.end(),
+        other.set_.begin(), other.set_.end(),
+        outinserter(temp, Fterator(nullptr)));
+    set_.swap(temp.set_);
+    seq_.swap(temp.seq_);
+	return *this;
+}
+
+void set_seq::display()
+{
+    std::cout << "\nSET (" << tag << "): ";
+    for(auto x : set_)
+        std::cout << x << ' ';
+    std::cout << "\nSEQUENCE: (" << tag << "): < ";
+    for (auto i : seq_)
+        std::cout << *i << ' ';
+    std::cout << ">\n";
+}
+
+void prepare_and(set_seq& one, set_seq& two, const int quantity)
+{
+	for (int i = 0; i < quantity; ++i)
+    {
+		int x = rand()%lim;
+        one.insert(x);
+        two.insert(x);
+	}
+}
+
+size_t set_seq::tags = 0;
+
+int main()
+{
+    using namespace std::chrono;
+    srand((unsigned int)1);
+    // srand((unsigned int)time(nullptr));
+    bool debug = false; //false, чтобы запретить отладочный вывод
+    auto MaxMul = 5;
+    int middle_power = 0, set_count = 0;
+    auto Used = [&] (set_seq & t){ middle_power += t.power();++set_count; };
+    auto DebOut = [debug] (set_seq & t) { if(debug) { t.display(); }};
+    auto rand = [] (int d) { return std::rand( )%d; }; //Лямбда-функция!
+    ofstream fout("in.txt"); //Открытие файла для результатов
+
+    int p = rand(20) + 1; //Текущая мощность (место для цикла по p)
+    for (int p = rand(20) + 1; p < 20; ++p)
+    {
+        //=== Данные ===
+        set_seq A(p), B(p), C(p), D(p), E(0), F(p);
+        int q_and(rand(MaxMul) + 1);
+        prepare_and(A, F, q_and);
+        if (debug) A.display(); Used(A);
+        if (debug) F.display(); Used(F);
+        //=== Цепочка операций ===
+        // (Операция пропускается (skipped!), если аргументы некорректны)
+        //Идёт суммирование мощностей множеств и подсчёт их количества,
+        // измеряется время выполнения цепочки
+        auto t1 = high_resolution_clock::now( );
+        if (debug) cout << "\n=== F&=A ===(" << q_and << ") ";
+        F&=A; DebOut(F); Used(F);
+
+        int q_sub(rand(MaxMul) + 1);
+        prepare_and(B, F, q_sub);
+        if (debug) F.display(), B.display(); middle_power += q_sub; Used(B);
+        if (debug) cout << "\n=== F-=B ===(" << q_sub << ") ";
+        F-=B; DebOut(F); Used(F);
+
+        int q_s_sub(rand(MaxMul) + 1);
+        prepare_and(C, F, q_s_sub);
+        if (debug) F.display(), C.display(); middle_power += q_s_sub; Used(C);
+        if (debug) cout << "\n=== F^=C ===(" << q_s_sub << ") ";
+        F-=C; DebOut(F); Used(F);
+
+        int a = rand(F.power()), b = rand(F.power());
+        if (debug) cout << "\n=== F.erase (" << a << "," << b << ")===";
+        if (debug && a>b) cout << "(skipped!)";
+        F.erase(a, b); DebOut(F); Used(F);
+
+        int e = rand(F.power());
+        if (debug) cout << "\n=== F.change (D, " << e << ") ===";
+        if (debug) D.display(); Used(D);
+        F.change(D, e); DebOut(F); Used(F);
+
+        if (debug) cout << "\n=== F.excl(E) ===";
+        E.prepare_excl(F);
+        if(debug && !E.power()) cout << "(skipped)!";
+        if(debug) E.display(); Used(E);
+        F.excl(E); DebOut(F); Used(F);
+
+        auto t2 = high_resolution_clock::now( );
+        auto dt = duration_cast<duration<double>>(t2-t1);
+        middle_power /= set_count;
+        fout << p << ' ' << dt.count() << endl; //Выдача в файл
+        cout << "\n=== Конец === (" << p << " : " << set_count << " * " <<
+        middle_power << " DT=" << (dt.count()) <<")\n";
+    }
+    cin.get();
+    return 0;
+}
