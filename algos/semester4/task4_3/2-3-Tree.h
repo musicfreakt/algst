2216d128e008b476042acc9ca9ac0202d60c7279
kivyfreakt <yaloveganikita@outlook.com>
2021-05-28 18:51:10 +0300

    Add 2-3tree realisation in task4

---
+++ algos/semester4/task4_3/2-3-Tree.h
@@ -0,0 +1,257 @@
+#ifndef TREE_H
+#define TREE_H
+
+using namespace std;
+
+struct Node 
+{
+	int key;
+	Node * next, *down;
+	Node() : down(nullptr), next(nullptr) {}
+	Node(int k) : key(k), down(nullptr), next(nullptr) {}
+	void Clear();
+};
+
+void Node::Clear()
+{
+	if (down) 
+	{
+		if (next->next) next->next->down->Clear();
+		next->down->Clear();
+		down->Clear();
+	}
+	else
+	{
+		if (next) 
+		{
+			if (next->next) delete next->next;
+			delete next;
+		}
+		delete this;
+	}
+}
+
+struct myiter : public std::iterator<std::forward_iterator_tag, int>
+{
+	myiter(Node *p) : Ptr(p) {}
+	myiter(Node *p, stack<pair<Node*, int> > St) : Ptr(p), St(St) {}
+	bool operator == (const myiter & Other) const { return Ptr == Other.Ptr; }
+	bool operator != (const myiter & Other) const { return !(*this == Other); }
+	myiter & operator++();
+	myiter operator++(int) { myiter temp(*this); ++*this; return temp; }
+	pointer operator->() { return &Ptr->key; }
+	reference operator*() { return Ptr->key; }
+	Node * Ptr;
+	stack<pair<Node*, int>> St;
+};
+
+class Tree 
+{
+	Node *root;
+	int h, count;
+
+	public:
+		Tree() : root(nullptr), h(0), count(0) {}
+		Tree(const Tree &other): Tree() {for(auto x = other.begin(); x != other.end(); ++x) insert(*x);}
+        Tree(Tree && other): Tree() {swap(other);}
+		~Tree(){if (root) root->Clear();}
+		void clear() { root->Clear(); root = nullptr; h = count = 0; }
+		void swap(Tree &);
+
+		myiter begin() const;
+		myiter end() const { return myiter(nullptr); }       
+		bool empty() const { return (root == nullptr); }
+        int size() const { return count; }
+        int height() const {return h;}
+
+		pair<myiter, bool> insert(int, myiter = myiter(nullptr));
+		myiter insert(myiter where, const int& k) { return insert(k, where).first; }
+		bool erase(int);
+
+		void Display();
+};
+
+void Tree::swap(Tree & other)
+{
+    std::swap(root, other.root);
+    std::swap(h, other.h);
+    std::swap(count, other.count);
+}
+
+myiter Tree::begin() const 
+{
+	stack<pair<Node*, int>>St;
+	Node* p = root;
+	if (p) {
+		St.push(make_pair(p, 1));
+		while (p->down) {
+			St.push(make_pair(p, 2));
+			p = p->down;
+		}
+	}
+	return myiter(p, St);
+}
+myiter& myiter::operator++()
+{
+	if (!Ptr)
+		return *this;
+	else
+	{
+		if (Ptr->next) 
+		{ 
+			Ptr = Ptr->next;
+			return (*this);
+		}
+		while (St.size()) 
+		{
+			Ptr = St.top().first;
+			int a = St.top().second;
+			St.pop();
+			switch (a) 
+			{
+			case 1:
+				Ptr = nullptr;
+				return (*this);
+			case 2:
+				St.push(make_pair(Ptr, 3));
+				Ptr = Ptr->next->down;
+				while (Ptr->down) 
+				{
+					St.push(make_pair(Ptr, 2));
+					Ptr = Ptr->down;
+				}
+				return (*this);
+			case 3:
+				if (Ptr->next->next) 
+				{
+					St.push(make_pair(Ptr, 4));
+					Ptr = Ptr->next->next->down;
+					while (Ptr->down) 
+					{
+						St.push(make_pair(Ptr, 2));
+						Ptr = Ptr->down;
+					}
+					return (*this);
+				}
+			case 4:;
+			}
+		}
+		Ptr = nullptr;
+		return (*this);
+	}
+}
+
+pair<myiter, bool> Tree::insert(int k, myiter where)
+{
+	Node *t(root);
+	int up = 0;
+	stack<pair<Node*, int>> St;
+	if (!where.Ptr) 
+	{
+		if (t == nullptr)
+		{	
+			root = new Node(k);
+			count = h = 1;
+			St.push(make_pair(root, 1));
+			return make_pair(myiter(root, move(St)), true);
+		}
+		else St.push(make_pair(root, 1));
+	}
+	else 
+	{
+		t = where.Ptr;
+		St = move(where.St);
+	}
+	while (1) 
+	{
+		if (k == t->key)
+			return make_pair(myiter(t, move(St)), false);
+		else if ((t->next) && (k == t->next->key))
+			return make_pair(myiter(t->next, move(St)), false);
+		else if ((t->next) && (t->next->next) && (k == t->next->next->key))
+			return make_pair(myiter(t->next->next, move(St)), false);
+		
+		if (t->down) 
+		{
+			if (k < t->next->key) 
+			{
+				if (k < t->key) t->key = k;
+				St.push(make_pair(t, ((t->next->next) ? 4 : 2)));
+				t = t->down;
+			}
+			else if (!(t->next->next) || t->next->next && k < t->next->next->key) 
+			{ 
+				St.push(make_pair(t, (t->next->next ? 5 : 3)));
+				t = t->next->down;
+			}
+			else 
+			{
+				St.push(make_pair(t, 6));
+				t = t->next->next->down;
+			}
+		}
+		else break;
+	}
+
+
+	Node *p = new Node(k); 
+	++count;
+	
+	if (!t->next || t->next && k < t->next->key)
+	 {
+		p->next = t->next;
+		t->next = p;
+		if (k < t->key) 
+			std::swap(t->key, p->key);
+	}
+	else if (!t->next->next || k < t->next->next->key) 
+	{ 
+		p->next = t->next->next;
+		t->next->next = p;
+	}
+	else t->next->next->next = p;
+	if (t->next && t->next->next && t->next->next->next) up = 1;
+
+	Node *new_node = p;
+
+	while (up) 
+	{
+		p = St.top().first;
+		int a = St.top().second;
+		Node * q = new Node(t->next->next->key);
+		switch (a) {
+		case 1:
+			up = 0;
+			h++;
+			root = St.top().first = new Node(root->key);
+			root->down = p;
+			root->next = q;
+			St.push(make_pair(root, 3));
+			break;
+		case 2:
+			up = 0;
+		case 4:
+			q->next = p->next; 
+			p->next = q;
+			break;
+		case 3:	
+			up = 0;
+		case 5:
+			q->next = p->next->next;
+			p->next->next = q;
+			St.top().second = 6;
+			break;
+		case 6:
+			p->next->next->next = q;
+		}
+		q->down = t->next->next;
+		t->next->next = nullptr;
+		t = q->down;
+		if (up)
+			St.pop(), t = p;
+	}
+	
+	return make_pair(myiter(new_node, move(St)), true);
+};
+
+# endif
---
+++ algos/semester4/task4_3/main.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stack>
+#include <iterator>
+#include <algorithm>
+#include <chrono>
+
+#include "2-3-Tree.h"
+#include "mycont.h"
+
+size_t mycont::tags = 0;
+
+int main()
+{
+    using namespace std::chrono;
+    ofstream fout("in.txt");
+    srand((unsigned int)time(nullptr));
+
+    auto MaxMul = 5;
+    int middle_power = 0, set_count = 0;
+    int iterations = 200;
+    int max_ = 2000;
+    bool debug = false;
+
+    auto Used = [&] (mycont & t){ middle_power += t.power(); ++set_count;};
+    auto rand = [] (int d) { return std::rand()%d; };
+    auto DebOut = [debug] (mycont & t) { if(debug) { t.display(); }};
+
+    for (int p = rand(max_) + 5; iterations > 0; --iterations, p = rand(max_) + 5)
+    {
+        int U = 3*p; // Устанавливаем мощность универсума
+        //=== Данные ===
+        mycont A(p, U), B(p, U), C(p, U), D(p, U), E(p, U), F(0, U);
+        //=== Цепочка операций ===
+        // (Операция пропускается (skipped!), если аргументы некорректны)
+        // Идёт суммирование мощностей множеств и подсчёт их количества,
+        // измеряется время выполнения цепочки
+        auto t1 = high_resolution_clock::now();
+
+        int q_and(rand(MaxMul) + 1);
+        prepare_and(A, F, q_and, U);
+        if (debug) A.display(); Used(A);
+        if (debug) F.display(); Used(F);
+        if (debug) cout << "\n=== F&=A ===(" << q_and << ") ";
+        F&=A; DebOut(F); Used(F);
+
+        int q_sub(rand(MaxMul) + 1);
+        prepare_and(B, F, q_sub, U);
+        if (debug) F.display(), B.display(); middle_power += q_sub; Used(B);
+        if (debug) cout << "\n=== F-=B ===(" << q_sub << ") ";
+        F-=B; DebOut(F); Used(F);
+
+        int q_s_sub(rand(MaxMul) + 1);
+        prepare_and(C, F, q_s_sub, U);
+        if (debug) F.display(), C.display(); middle_power += q_s_sub; Used(C);
+        if (debug) cout << "\n=== F^=C ===(" << q_s_sub << ") ";
+        F^=C; DebOut(F); Used(F);
+
+        int a, b;
+        if (F.power()) a = rand(F.power()), b = rand(F.power());
+        else a = b = 0;
+        if (debug) cout << "\n=== F.erase (" << a << "," << b << ")===";
+        if (a>b) swap(a, b);
+        F.erase(a, b); DebOut(F); Used(F);
+
+        if (debug) cout << "\n=== F.concat(E) ===";
+        if (debug) E.display(), F.display(); Used(E);
+        F.concat(E); DebOut(F); Used(F);
+
+        if (debug) cout << "\n=== F.excl(E) ===";
+        if(debug && !E.power()) cout << "(skipped)!";
+        if(debug) E.display(); Used(E);
+        F.excl(E); DebOut(F); Used(F);
+
+        int d = rand(F.power());
+	    if (debug) cout << "\n=== F.subst (D, " << d << ") ===";
+	    if (debug) D.display(); Used(D);
+	    F.subst(D, d); DebOut(F); Used(F);
+
+        auto t2 = high_resolution_clock::now();
+        auto dt = duration_cast<duration<double>>(t2-t1);
+        middle_power /= set_count;
+        if (!debug) fout << middle_power << ' ' << dt.count() << endl;
+        cout << "\n=== Конец === (" << p << " : " << set_count << " * " <<
+        middle_power << " DT=" << (dt.count()) <<")\n";
+        middle_power = 0; set_count = 0;
+    }
+    return 0;
+}
