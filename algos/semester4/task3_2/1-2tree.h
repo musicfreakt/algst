3813671f4c24b0ae106731f46ad9094d511c5cf8
kivyfreakt <yaloveganikita@outlook.com>
2021-06-02 16:15:55 +0300

    change container structure

--- algos/semester4/task3_2/1-2tree.h
+++ algos/semester4/task3_2/1-2tree.h
@@ -8,7 +8,6 @@ struct Node
 	int key;
 	int N = 1;
 	bool next = false;
-	int duplicates;
 	Node *left, *right;
 
 	Node() : left(nullptr), right(nullptr) {}
@@ -53,35 +52,86 @@ struct tree_iter : public std::iterator<std::forward_iterator_tag, int>
 	MyStack St;
 };
 
+// ИТЕРАТОР ВСТАВКИ (нужны присваивание, разыменование , инкремент)
+template<typename Container, typename Iter = tree_iter>
+class outiterator: public iterator<output_iterator_tag, typename Container::value_type>
+{
+    protected:
+        Container& container; // Контейнер для вставки элементов
+        Iter iter; // текущее значение итератора чтения
+    public:
+        outiterator(Container &c, Iter it) : container(c), iter(it) {}
+        const outiterator<Container>& operator= (const typename Container::value_type& value)
+        {
+            iter = container.insert(iter, value);
+            return *this;
+        }
+
+        // Присваивание копии фиктивное
+        const outiterator<Container>& operator= (const outiterator<Container> &) { return *this; }
+        outiterator<Container>& operator* () {return *this;} // Разыменование - пустая операция
+        outiterator<Container>& operator++ () {return *this;} // Инкремент - пустая операция
+        outiterator<Container>& operator++ (int) {return *this;} // Инкремент - пустая операция
+};
+
+// функция для создания итератора вставки
+template<typename Container, typename Iter>
+inline outiterator<Container, Iter> outinserter(Container& c, Iter It)
+{
+    return outiterator<Container, Iter>(c, It);
+}
+
 class Tree 
 {
+	static int tags;
+	char tag;
 	Node * root;
 	int count;
+
+	vector <int> keys;
 	public:
+	 	using key_type = int;
+        using value_type = int;
+        using key_compare = less<int>;
 		static int height;
-		// конструкторы/деструкторы:
-		Tree() : root(nullptr), count(0){}
+		
+		Tree() : root(nullptr), count(0), tag('A' + tags++) {}
 		Tree(const Tree &other): Tree() {for(auto x : other) insert(x);}
         Tree(Tree && other): Tree() {swap(other);}
 		~Tree() { delete root; };
 
-		// доступ:
 		tree_iter begin() const;
 		tree_iter end() const { return tree_iter(nullptr); }
 		int size() const { return count; }
 		bool empty() const { return (root == nullptr); }
+		size_t power() const { return keys.size(); } 
 		void swap(Tree &);
 
-		// вставка/удаление
 		pair<tree_iter, bool> insert(int, tree_iter = tree_iter(nullptr));
 		tree_iter insert(const tree_iter where, const int& k) { return insert(k, where).first; }
 		bool erase(int);
 		void clear() { count = 0; delete root; root = nullptr; }
 
-		// другие методы
 		void display();	
 		Tree & operator= (const Tree &);
-        Tree & operator= (Tree && other) {swap(other); return *this;}
+        Tree & operator= (Tree &&);
+
+        void merge(const Tree&);
+        void concat(const Tree&);
+        void erase(int, int);
+
+        Tree& operator &= (const Tree &);
+        Tree operator & (const Tree & other) const
+        { Tree result(*this); return (result &= other); }
+        Tree& operator |= (const Tree &);
+        Tree operator | (const Tree & other) const
+        { Tree result(*this); return (result |= other); }
+        Tree& operator -= (const Tree &);
+        Tree operator - (const Tree & other) const
+        {Tree result(*this); return (result -= other);}
+        Tree& operator ^= (const Tree &);
+        Tree operator ^ (const Tree & other) const
+        {Tree result(*this); return (result ^= other);}
 };
 
 tree_iter Tree::begin() const
@@ -127,6 +177,7 @@ void Tree::swap(Tree & other)
     std::swap(root, other.root);
     std::swap(height, other.height);
     std::swap(count, other.count);
+	std::swap(keys, other.keys);
 }
 
 pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
@@ -137,16 +188,15 @@ pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
 
 	if (!where.Ptr) 
 	{
-
 		if (t == nullptr) 
 		{
 			root = new Node(k);
+			keys.push_back(k);
 			count = height = 1;
 			St.push(make_pair(root, 1));
 			return make_pair(tree_iter(root, move(St)), true);
 		}
 		else St.push(make_pair(root, 1));
-
 	}
 	else 
 	{
@@ -168,6 +218,7 @@ pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
 			else 
 			{
 				t->left = new Node(k, nullptr, t->left);
+				keys.push_back(k);
 				new_node = t->left;
 				cont = false;
 			}
@@ -175,6 +226,7 @@ pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
 		else if (!t->right) 
 		{
 			t->right = new Node(k);
+			keys.push_back(k);
 			t->next = true;
 			new_node = t->right;
 			cont = false;
@@ -191,6 +243,7 @@ pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
 				else 
 				{
 					t->right->left = new Node(k, nullptr, t->right->left);
+					keys.push_back(k);
 					new_node = t->right->left;
 					cont = false;
 				}
@@ -205,6 +258,7 @@ pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
 				else 
 				{
 					t->right->right = new Node(k);
+					keys.push_back(k);
 					up = t->right->next = true;
 					new_node = t->right->right;
 					cont = false;
@@ -219,6 +273,7 @@ pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
 		else 
 		{
 			t->right = new Node(k);
+			keys.push_back(k);
 			t->next = true;
 			new_node = t->right;
 			cont = false;
@@ -235,7 +290,7 @@ pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
 			t->left = t1;
 			t1 = t;
 			t = St.top().first;
-			new_node = t1;
+
 			switch (St.top().second) 
 			{
 				case 1:
@@ -269,7 +324,8 @@ pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
 					t->right->next = true;
 					t1->next = false;
 			}
-			if (up) St.pop();
+			if (up) 
+				St.pop();
 		}
 	}
 
@@ -277,66 +333,21 @@ pair<tree_iter, bool> Tree::insert(int k, tree_iter where)
 	return make_pair(tree_iter(new_node, move(St)), true);
 };
 
-// bool Tree::erase(int k)
-// {
-
-// 	Node *p{ nullptr }, *q{ root };
-// 	int a{ 0 };
-// 	bool cont{ q != nullptr };
-
-// 	while (cont && (k != q->key)) {
-// 		p = q;
-// 		if (a = k > q->key)
-// 			q = q->right;
-// 		else q = q->left;
-// 		cont = (q != nullptr);
-// 	}
-// 	if (cont) {
-// 		Node *r{ root };
-
-// 		if (r = q->right) {
-// 			if (r->left) {
-// 				p = q;
-// 				a = 1;
-// 				do {
-// 					p = r;
-// 					r = r->left;
-// 					a = 0;
-// 				} while (r->left);
-// 				q->key = r->key;
-// 				p->left = r->right;
-// 				q = r;
-// 			}
-// 			else {
-// 				(a ? r->right : r->left) = q->left;
-// 				if (p) (a ? p->right : p->left) = r;
-// 				else root = r;
-// 			}
-// 		}
-// 		else {
-// 			if (p) {
-// 				if (a) {
-// 					p->right = q->left; p->next = false;
-// 				}
-// 				else  p->left = q->left;
-// 			}
-// 			else root = q->left;
-// 		}
-// 		q->left = q->right = nullptr;
-// 		--count;
-// 		delete q;
-// 		return true;
-// 	}
-// 	else return false;
-// }
 
 Tree & Tree::operator= (const Tree & other)
 {
     Tree temp;
-    for (auto x: other)
-        temp.insert(x);
+	std::copy(other.begin(), other.end(), outinserter(temp, tree_iter(nullptr)));
     swap(temp);
     return *this;
+}
+
+Tree& Tree::operator=(Tree&& other)
+{
+	swap(other);
+	for (auto x : other.keys)
+		keys.push_back(x);
+	return *this;
 }
 
 void Tree::display()
@@ -349,6 +360,8 @@ void Tree::display()
 	SCREEN[0] += (root) ? to_string(root->key) : " <Empty>";
 	SCREEN[0].resize(MAXCOL, '.');
 	showscr();
+
+	cout << "< "; for (auto x : keys) cout << x << " "; cout << ">\n\n"; 
 }
 
 void Node::display(int level, int col)
@@ -364,6 +377,80 @@ void Node::display(int level, int col)
 	else
 		if (right) right->display(level + 1, col + (OFFSET[level + 1]));
 	if (level > Tree::height) Tree::height = level;
+}
+
+Tree& Tree::operator&= (const Tree & other)
+{
+    Tree temp;
+    set_intersection(begin(), end(),
+        other.begin(), other.end(),
+        outinserter(temp, tree_iter(nullptr)));
+    swap(temp);
+	return *this;
+}
+
+Tree& Tree::operator|= (const Tree & other)
+{
+    Tree temp;
+    set_union(begin(), end(),
+        other.begin(), other.end(),
+        outinserter(temp, tree_iter(nullptr)));
+    swap(temp);
+	return *this;
+}
+
+Tree& Tree::operator-= (const Tree & other)
+{
+    Tree temp;
+    set_difference(begin(), end(),
+        other.begin(), other.end(),
+        outinserter(temp, tree_iter(nullptr)));
+    swap(temp);
+	return *this;
+}
+
+Tree& Tree::operator^= (const Tree & other)
+{
+    Tree temp;
+    set_symmetric_difference(begin(), end(),
+        other.begin(), other.end(),
+        outinserter(temp, tree_iter(nullptr)));
+    swap(temp);
+	return *this;
+}
+
+void Tree::erase(int left, int right)
+{
+    int p = power();
+    left = std::min(left, p);
+    right = std::min(right+1, p);
+    if(left <= right)
+    {
+        Tree temp;
+        for(int i = 0; i < left; ++i)
+			temp.insert(keys[i]);
+        for(int i = right; i < p; ++i)
+            temp.insert(keys[i]);
+        swap(temp);
+    }
+}
+
+void Tree::concat(const Tree & other)
+{
+    for(auto x : other.keys)
+        insert(x);
+}
+
+void Tree::merge(const Tree & other) 
+{ 
+	using std::sort;
+	vector <int> temp(other.keys), res;
+	// auto le = [ ] ( a, b)->bool { return *a < *b; };
+	std::merge(keys.begin(), keys.end(), temp.begin(), temp.end(), 
+        std::back_inserter(res));
+    for (auto x : other.keys)
+	    insert(x);
+	keys.swap(res);
 }
 
 #endif
--- algos/semester4/task3_2/main.cpp
+++ algos/semester4/task3_2/main.cpp
@@ -8,81 +8,175 @@
 
 #include "screen.h"
 #include "1-2tree.h"
-#include "mycont.h"
 
-const int N = 52;
+const int N = 10; // 52
 int Tree::height = 0;
-size_t mycont::tags = 0;
+int Tree::tags = 0;
+
+void createkeys(vector<int>& a, int n) 
+{
+    int x;
+
+    for (int i = 0; i < n; i++)
+    {
+        x = { rand() % (n + 20) + 1 };
+        while (find(a.begin(), a.end(), x) != a.end())
+            x = { rand() % (n + 20) + 1 };
+        a.push_back(x);
+
+    }
+}
+
 
 int main()
 {
     std::srand(time(nullptr));
-    int menu_item = 2;
-
-    // std::cout << "1) Test example\n2) Random data\n> ";
-    // cin >> menu_item;
-    if (menu_item == 1)
-    {
-        // в примере мощность множеств мала для большей наглядности
-        std::vector<int> a_ = {3, 2, 1},
-        b_ = {1, 3, 4, 5},
-        c_ = {1, 3, 8},
-        d_ = {14, 1, 3, 8, 23, 14, 12},
-        e_ = {1, 1, 0, 3, 6, 5, 4};
-
-        mycont a(a_.begin(), a_.end()),
-        b(b_.begin(), b_.end()),
-        c(c_.begin(), c_.end()),
-        d(d_.begin(), d_.end()),
-        e(e_.begin(), e_.end()),
-        f;
-
-        a.display(); b.display(); c.display(); d.display(); e.display();
-
-        cout << "\n\nSet operations example: \n\n";
-        f = a - (b & c & d) ^ e;
-
-        f.display(true);
-        // cout << "\n\nSequence operations example: \n\n";
-        // cout << "Erase elements from 2 to 4 in (e):";
-        // e.erase(2, 4);
-        // e.display(true);
-
-        // cout << "\nExcl (c) from (d):";
-        // d.excl(c);
-        // d.display(true);
-
-        // cout << "\nChange (c) in (d) from 3 position:";
-        // d.change(c, 3);
-        // d.display(true);
-
-    }
-    else
-    {
-		mycont a(10);
-
-		a.display(true);
-
-        // mycont a(N), b(N), c(N), d(N), e(N), f;
-
-        // a.display(); b.display(); c.display(); d.display(); e.display();
-
-        // cout << "\n\nSet operations example: \n\n";
-        // f = (a - b) | c ^ d & e;
-
-        // f.display();
-
-		// cout << "\n\nSequence operations example: \n\n";
-		// merge
-
-		// concat
-
-		// erase
-        
-        // cout << "Erase elements from 2 to 4 in (a):";
-        // a.display();
-        // a.erase(2, 4);
-        // a.display();
-    }
+    
     return 0;
 }
+
+// // Выражение A & B ^ C & D & E
+
+// int k = 0, l = 0, N = 0;
+
+// vector<int> A, B, C, D, E;
+
+// int p = rand() % (10) + 5;
+
+// cout << "Подготовка множеств длины " << p << endl;
+
+// createkeys(A, p);
+
+// createkeys(B, p);
+
+// createkeys(C, p);
+
+// createkeys(D, p);
+
+// createkeys(E, p);
+
+// cout << "Множества успешно созданы" << endl;
+
+// system("pause");
+
+// Tree first, second, third, fourth, fifth, answer1, answer2, answer3, answer4;
+
+// for (auto x : A) {
+
+// first.insert(x);
+
+// }
+
+// first.Display("keys A");
+
+// system("pause");
+
+// for (auto x : B) {
+
+// second.insert(x);
+
+// }
+
+// second.Display("keys B");
+
+// system("pause");
+
+// for (auto x : C) {
+
+// third.insert(x);
+
+// }
+
+// third.Display("keys C");
+
+// system("pause");
+
+// for (auto x : D) {
+
+// fourth.insert(x);
+
+// }
+
+// fourth.Display("keys D");
+
+// system("pause");
+
+// for (auto x : E) {
+
+// fifth.insert(x);
+
+// }
+
+// fifth.Display("keys E");
+
+// system("pause");
+
+// answer1 = first & second;
+
+// answer1.Display("A & B");
+
+// system("pause");
+
+// answer2 = third & fourth;
+
+// answer2.Display("C & D");
+
+// system("pause");
+
+// answer3 = answer2 & fifth;
+
+// answer3.Display("C & D & E");
+
+// system("pause");
+
+// answer4 = answer1 ^ answer3;
+
+// answer4.Display("A & B ^ C & D & E");
+
+// system("pause");
+
+// size_t pos;
+
+// pos = rand() % (first.Power() + 1 ) + 1;
+
+// cout << "операция CHANGE будет выполняться с позиции: " << pos << endl;
+
+// first.CHANGE(second, pos);
+
+// first.Display("A CHANGE B");
+
+// system("pause");
+
+// cout << "Операция EXCL будет использовать CHANGE вместо множества B: " << endl;
+
+// first.EXCL(second);
+
+// first.Display("A EXCL B");
+
+// system("pause");
+
+// pos = rand() % (third.Power() + 1) + 0;
+
+// cout << "операция SUBST будет выполняться с позиции: " << pos << endl;
+
+// third.SUBST(fourth, pos);
+
+// string subs = "C SUBST D (" + to_string(pos) + ")";
+
+// third.Display(subs);
+
+// system("pause");
+
+// pos = rand() % (4) + 1;
+
+// cout << "операция MUL будет выполняться " << pos << " раз(а)" << endl;
+
+// fifth.MUL(pos);
+
+// string mul = "E MUL (" + to_string(pos) + ")";
+
+// fifth.Display(mul);
+
+// return 0;
+
+// } 
