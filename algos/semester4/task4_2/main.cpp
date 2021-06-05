c0b2ad2b65bcc4f87355d981f0d00d023fcf9b0b
kivyfreakt <yaloveganikita@outlook.com>
2021-06-05 12:19:45 +0300

    Add new realisation of task4

---
+++ algos/semester4/task4_2/main.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stack>
+#include <iterator>
+#include <algorithm>
+#include <chrono>
+
+#include "mycont.h"
+
+int mycont::height = 0;
+int mycont::tags = 0;
+
+int main()
+{
+    using namespace std::chrono;
+    ofstream fout("in.txt");
+    srand((unsigned int)time(nullptr));
+
+    auto MaxMul = 5;
+    int middle_power = 0, set_count = 0;
+    int iterations = 200;
+    int max_ = 2000;
+    bool debug = false;
+
+    auto Used = [&] (mycont & t){ middle_power += t.power(); ++set_count;};
+    auto rand = [] (int d) { return std::rand()%d; };
+    auto DebOut = [debug] (mycont & t) { if(debug) { t.display(); }};
+
+    for (int p = rand(max_) + 5; iterations > 0; --iterations, p = rand(max_) + 5)
+    {
+        int U = 4*p; // Устанавливаем мощность универсума
+        //=== Данные ===
+        mycont A(p, U), B(p, U), C(p, U), D(p, U), E(p, U), F(0, U);
+        //=== Цепочка операций ===
+        auto t1 = high_resolution_clock::now();
+
+        int q_and(rand(MaxMul) + 1);
+        prepare_and(A, F, q_and, U);
+        if (debug) A.display(); Used(A);
+        if (debug) F.display(); Used(F);
+        if (debug) cout << "\n=== F&=A ===(" << q_and << ") ";
+        F&=A; DebOut(F); Used(F);
+
+        int q_sub(rand(MaxMul) + 1);
+        prepare_and(B, F, q_sub, U);
+        if (debug) F.display(), B.display(); middle_power += q_sub; Used(B);
+        if (debug) cout << "\n=== F-=B ===(" << q_sub << ") ";
+        F-=B; DebOut(F); Used(F);
+
+        int q_s_sub(rand(MaxMul) + 1);
+        prepare_and(C, F, q_s_sub, U);
+        if (debug) F.display(), C.display(); middle_power += q_s_sub; Used(C);
+        if (debug) cout << "\n=== F^=C ===(" << q_s_sub << ") ";
+        F^=C; DebOut(F); Used(F);
+
+        int a, b;
+        if (F.power()) a = rand(F.power()), b = rand(F.power());
+        else a = b = 0;
+        if (debug) cout << "\n=== F.erase (" << a << "," << b << ")===";
+        if (a>b) swap(a, b);
+        F.erase(a, b); DebOut(F); Used(F);
+
+        if (debug) cout << "\n=== F.merge(D) ===";
+        if (debug) D.display(), F.display(); Used(D);
+        F.merge(D); DebOut(F); Used(F);
+
+        if (debug) cout << "\n=== F.concat(E) ===";
+        if (debug) E.display(), F.display(); Used(E);
+        F.concat(E); DebOut(F); Used(F);
+
+        auto t2 = high_resolution_clock::now();
+        auto dt = duration_cast<duration<double>>(t2-t1);
+        middle_power /= set_count;
+        if (!debug) fout << middle_power << ' ' << dt.count() << endl;
+        cout << "\n=== Конец === (" << p << " : " << set_count << " * " <<
+        middle_power << " DT=" << (dt.count()) <<")\n";
+        middle_power = 0; set_count = 0;
+    }
+    return 0;
+}
---
+++ algos/semester4/task4_2/mycont.h
@@ -0,0 +1,452 @@
+#ifndef MYCONT_H
+#define MYCONT_H
+
+using namespace std;
+
+struct node 
+{
+	int key;
+	int N = 1;
+	bool next = false;
+	node *left, *right;
+
+	node() : left(nullptr), right(nullptr) {}
+	node(int k, node * l = nullptr, node * r = nullptr) : key(k), left(l), right(r) {}
+	~node() { delete right; delete left; };
+};
+
+using MyStack = stack<pair<node*, int>>;
+
+struct Iterator : public std::iterator<std::forward_iterator_tag, int>
+{
+	Iterator(node *p) : Ptr(p) {}
+	Iterator(node *p, MyStack St) : Ptr(p), St(St) {}
+	bool operator == (const Iterator & Other) const { return Ptr == Other.Ptr; }
+	bool operator != (const Iterator & Other) const { return !(*this == Other); }
+	Iterator & operator++();
+	Iterator operator++(int) { Iterator temp(*this); ++*this; return temp; }
+	pointer operator->() { return &Ptr->key; }
+	reference operator*() { return Ptr->key; }
+	node * Ptr;
+	MyStack St;
+};
+
+// ИТЕРАТОР ВСТАВКИ (нужны присваивание, разыменование , инкремент)
+template<typename Container, typename Iter = Iterator>
+class outiterator: public iterator<output_iterator_tag, typename Container::value_type>
+{
+    protected:
+        Container& container; // Контейнер для вставки элементов
+        Iter iter; // текущее значение итератора чтения
+    public:
+        outiterator(Container &c, Iter it) : container(c), iter(it) {}
+        const outiterator<Container>& operator= (const typename Container::value_type& value)
+        {
+            iter = container.insert(iter, value);
+            return *this;
+        }
+
+        // Присваивание копии фиктивное
+        const outiterator<Container>& operator= (const outiterator<Container> &) { return *this; }
+        outiterator<Container>& operator* () {return *this;} // Разыменование - пустая операция
+        outiterator<Container>& operator++ () {return *this;} // Инкремент - пустая операция
+        outiterator<Container>& operator++ (int) {return *this;} // Инкремент - пустая операция
+};
+
+// функция для создания итератора вставки
+template<typename Container, typename Iter>
+inline outiterator<Container, Iter> outinserter(Container& c, Iter It)
+{
+    return outiterator<Container, Iter>(c, It);
+}
+
+class mycont 
+{
+	static int tags;
+	char tag;
+	node * root;
+	int count;
+
+	vector <int> keys;
+	public:
+	 	using key_type = int;
+        using value_type = int;
+        using key_compare = less<int>;
+		static int height;
+		
+		mycont() : root(nullptr), count(0), tag('A' + tags++) {}
+		mycont(const mycont &other): mycont() {for(auto x : other) insert(x);}
+        mycont(mycont && other): mycont() {swap(other);}
+		template <typename iter>
+        mycont(iter, iter);
+		mycont(int, int);
+		~mycont() { delete root; };
+
+		Iterator begin() const;
+		Iterator end() const { return Iterator(nullptr); }
+		int size() const { return count; }
+		bool empty() const { return (root == nullptr); }
+		size_t power() const { return keys.size(); } 
+		void swap(mycont &);
+
+		pair<Iterator, bool> insert(int, Iterator = Iterator(nullptr));
+		Iterator insert(const Iterator where, const int& k) { return insert(k, where).first; }
+		bool erase(int);
+		void clear() { count = 0; delete root; root = nullptr; }
+
+		void display(bool);	
+		mycont & operator= (const mycont &);
+        mycont & operator= (mycont &&);
+
+        void merge(const mycont&);
+        void concat(const mycont&);
+        void erase(int, int);
+
+        mycont& operator &= (const mycont &);
+        mycont operator & (const mycont & other) const
+        { mycont result(*this); return (result &= other); }
+        mycont& operator |= (const mycont &);
+        mycont operator | (const mycont & other) const
+        { mycont result(*this); return (result |= other); }
+        mycont& operator -= (const mycont &);
+        mycont operator - (const mycont & other) const
+        {mycont result(*this); return (result -= other);}
+        mycont& operator ^= (const mycont &);
+        mycont operator ^ (const mycont & other) const
+        {mycont result(*this); return (result ^= other);}
+
+		friend void prepare_and(mycont&, mycont&, const int, int);
+};
+
+mycont::mycont(int power, int lim) : mycont()
+{
+    for(int i = 0; i < power; ++i)
+    {
+        int a = std::rand()%lim;
+        insert(a);
+    }
+}
+
+template <typename iter>
+mycont::mycont(iter b, iter e): mycont()
+{
+    for (auto i = b; i != e; ++i)
+        insert(*i);
+}
+
+Iterator mycont::begin() const
+{
+	stack<pair<node*, int>>St;
+	node* p(root);
+	if (p) {
+		while (p->left) {
+			St.push(make_pair(p, 0));
+			p = p->left;
+		}
+	}
+	return Iterator(p, St);
+}
+
+Iterator& Iterator::operator++()
+{
+	if (!Ptr) 
+	{
+		return *this;
+	}
+	if (Ptr->right) {
+		St.push(make_pair(Ptr, 1));
+		Ptr = Ptr->right;
+		while (Ptr->left)
+		{
+			St.push(make_pair(Ptr, 0));
+			Ptr = Ptr->left;
+		}
+	}
+	else {
+		pair<node*, int> pp(Ptr, 1);
+		while (!St.empty() && pp.second) { pp = St.top(); St.pop(); }
+		if (pp.second)
+			Ptr = nullptr;
+		else Ptr = pp.first;
+	}
+	return (*this);
+}
+
+void mycont::swap(mycont & other)
+{
+    std::swap(root, other.root);
+    std::swap(height, other.height);
+    std::swap(count, other.count);
+	std::swap(keys, other.keys);
+}
+
+pair<Iterator, bool> mycont::insert(int k, Iterator where)
+{
+	node *t{ root }, *new_node{nullptr};
+	bool cont{ true }, up{ false };
+	stack<pair<node*, int>> St;
+
+	if (!where.Ptr) 
+	{
+		if (t == nullptr) 
+		{
+			root = new node(k);
+			keys.push_back(k);
+			count = height = 1;
+			St.push(make_pair(root, 1));
+			return make_pair(Iterator(root, move(St)), true);
+		}
+		else St.push(make_pair(root, 1));
+	}
+	else 
+	{
+		t = where.Ptr;
+		St = move(where.St);
+	}
+
+	while (cont) 
+	{
+		if (k == t->key)
+		{
+			keys.push_back(k);
+			return make_pair(Iterator(t, move(St)), false);
+		}
+		if (k < t->key) 
+		{
+			if (t->left) 
+			{
+				St.push(make_pair(t, 2));
+				t = t->left;
+			}
+			else 
+			{
+				t->left = new node(k, nullptr, t->left);
+				keys.push_back(k);
+				new_node = t->left;
+				cont = false;
+			}
+		}
+		else if (!t->right) 
+		{
+			t->right = new node(k);
+			keys.push_back(k);
+			t->next = true;
+			new_node = t->right;
+			cont = false;
+		}
+		else if (t->next) 
+		{
+			if (k < t->right->key) 
+			{
+				if (t->right->left) 
+				{
+					St.push(make_pair(t, 3));
+					t = t->right->left;
+				}
+				else 
+				{
+					t->right->left = new node(k, nullptr, t->right->left);
+					keys.push_back(k);
+					new_node = t->right->left;
+					cont = false;
+				}
+			}
+			else 
+			{
+				if (t->right->right) 
+				{
+					St.push(make_pair(t, 4));
+					t = t->right->right;
+				}
+				else 
+				{
+					t->right->right = new node(k);
+					keys.push_back(k);
+					up = t->right->next = true;
+					new_node = t->right->right;
+					cont = false;
+				}
+			}
+		}
+		else if (t->right) 
+		{
+			St.push(make_pair(t, 3));
+			t = t->right;
+		}
+		else 
+		{
+			t->right = new node(k);
+			keys.push_back(k);
+			t->next = true;
+			new_node = t->right;
+			cont = false;
+		}
+
+		while (up) 
+		{
+			std::swap(t->key, t->right->key);
+			node * t1{ t->right };
+			t->next = t1->next = false;
+			t->right = t->right->right;
+			t1->right = t1->left;
+			t1->left = t->left;
+			t->left = t1;
+			t1 = t;
+			t = St.top().first;
+
+			switch (St.top().second) 
+			{
+				case 1:
+					++height;
+					up = false;
+					break;
+				case 2:
+					std::swap(t->key, t1->key);
+					t->left = t1->left;
+					t1->left = t1->right;
+					t1->right = t->right;
+					t->right = t1;
+					up = t1->next = t->next;
+					break;
+				case 3:
+					if (t->next) 
+					{
+						t->right->left = t1->right;
+						t1->right = t->right;
+						t->right = t1;
+						t1->next = true;
+					}
+					else 
+					{
+						t->next = true;
+						up = t1->next = false;
+						St.pop();
+					}
+					break;
+				case 4:
+					t->right->next = true;
+					t1->next = false;
+			}
+			if (up) 
+				St.pop();
+		}
+	}
+
+	++count;
+	return make_pair(Iterator(new_node, move(St)), true);
+};
+
+
+mycont & mycont::operator= (const mycont & other)
+{
+    mycont temp;
+	std::copy(other.begin(), other.end(), outinserter(temp, Iterator(nullptr)));
+    swap(temp);
+    return *this;
+}
+
+mycont& mycont::operator=(mycont&& other)
+{
+	swap(other);
+	for (auto x : other.keys)
+		keys.push_back(x);
+	return *this;
+}
+
+void mycont::display(bool flag = false)
+{
+	cout << "МНОЖЕСТВО( " << tag << " ) : ";
+	for (auto x = begin(); x != end(); ++x)
+		cout << *x << " ";
+
+	cout << "\nПОСЛЕДОВАТЕЛЬНОСТЬ ( " << tag << " ): < "; for (auto x : keys) cout << x << " "; cout << ">\n\n"; 
+}
+
+mycont& mycont::operator&= (const mycont & other)
+{
+    mycont temp;
+    set_intersection(begin(), end(),
+        other.begin(), other.end(),
+        outinserter(temp, Iterator(nullptr)));
+    swap(temp);
+	return *this;
+}
+
+mycont& mycont::operator|= (const mycont & other)
+{
+    mycont temp;
+    set_union(begin(), end(),
+        other.begin(), other.end(),
+        outinserter(temp, Iterator(nullptr)));
+    swap(temp);
+	return *this;
+}
+
+mycont& mycont::operator-= (const mycont & other)
+{
+    mycont temp;
+    set_difference(begin(), end(),
+        other.begin(), other.end(),
+        outinserter(temp, Iterator(nullptr)));
+    swap(temp);
+	return *this;
+}
+
+mycont& mycont::operator^= (const mycont & other)
+{
+    mycont temp;
+    set_symmetric_difference(begin(), end(),
+        other.begin(), other.end(),
+        outinserter(temp, Iterator(nullptr)));
+    swap(temp);
+	return *this;
+}
+
+void mycont::erase(int left, int right)
+{
+    int p = power();
+    left = std::min(left, p);
+    right = std::min(right+1, p);
+    if(left <= right)
+    {
+        mycont temp;
+        for(int i = 0; i < left; ++i)
+			temp.insert(keys[i]);
+        for(int i = right; i < p; ++i)
+            temp.insert(keys[i]);
+        swap(temp);
+    }
+}
+
+void mycont::concat(const mycont & other)
+{
+    for(auto x : other.keys)
+        insert(x);
+}
+
+void mycont::merge(const mycont & other) 
+{ 
+	using std::sort;
+	
+	vector <int> temp(other.keys), res;
+	std::merge(keys.begin(), keys.end(), temp.begin(), temp.end(), 
+        std::back_inserter(res));
+    for (auto x : other.keys)
+	{
+		insert(x);
+	    res.push_back(x);
+	}
+	keys.swap(res);
+}
+
+void prepare_and(mycont& one, mycont& two, const int quantity, int lim)
+{
+	for (int i = 0; i < quantity; ++i)
+    {
+		int x = rand()%lim;
+        one.insert(x);
+        two.insert(x);
+	}
+}
+
+#endif
