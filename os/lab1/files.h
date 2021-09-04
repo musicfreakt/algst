f02236207486b6e1182b560dc4cfa282c42ecfc8
Nikita Yalovega <yaloveganikita@outlook.com>
2021-09-04 18:57:56 +0300

    Update files.h

--- os/lab1/files.h
+++ os/lab1/files.h
@@ -1,10 +1,6 @@
-#define _WIN32_WINNT 0x501
-
 #include <iostream>
 #include <windows.h>
 #include <io.h>
-#include <stdio.h>
-// #include <tchar.h>
 #include <string>
 #include <sstream>
 
@@ -18,8 +14,3 @@ void moveFile();
 void fileInfo();
 void changeFileAttributes();
 void changeCreationTime();
-
-
-#if (_WIN32 >= 0x0500)
-WINBASEAPI BOOL WINAPI GetFileSizeEx(HANDLE, PLARGE_INTEGER);
-#endif
